56317912e93c7ffb3b0b01cbc0217f8098f148ec
Kasualix <670067575@qq.com>
2023-06-29 20:56:50 +0800

    Update BaseAttribute.java

--- src/main/java/com/xiaohunao/equipmentbenediction/attribute/BaseAttribute.java
+++ src/main/java/com/xiaohunao/equipmentbenediction/attribute/BaseAttribute.java
@@ -1,15 +1,16 @@
 package com.xiaohunao.equipmentbenediction.attribute;
 
+import com.xiaohunao.equipmentbenediction.EquipmentBenediction;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 
 public class BaseAttribute extends RangedAttribute {
     public BaseAttribute(String name, double defaultValue, double minValue, double maxValue) {
-        super("attribute.name." + name, defaultValue, minValue, maxValue);
+        super("attribute." + EquipmentBenediction.MOD_ID + "." + name, defaultValue, minValue, maxValue);
         this.setSyncable(true);
     }
 
     public BaseAttribute(String name) {
-        super("attribute.name." + name, 0.0, 0.0, 1.0);
+        super("attribute." + EquipmentBenediction.MOD_ID + "." + name, 0.0, 0.0, 1.0);
         this.setSyncable(true);
     }
 }
