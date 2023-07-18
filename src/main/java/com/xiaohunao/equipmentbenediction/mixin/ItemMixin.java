88c7f303e0f321b8941204d303061a13e0f815fd
XiaoHuNao <shiwenbin2250@gmail.com>
2023-07-18 13:49:00 +0800

    compatible Curios

--- src/main/java/com/xiaohunao/equipmentbenediction/mixin/ItemMixin.java
+++ src/main/java/com/xiaohunao/equipmentbenediction/mixin/ItemMixin.java
@@ -10,16 +10,19 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.fml.ModList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.util.ICuriosHelper;
 
 import java.util.List;
+import java.util.Set;
 
 @Mixin(Item.class)
 public class ItemMixin {
-
     @Inject(method = "inventoryTick", at = @At(value = "HEAD"))
     public void mark(ItemStack stack, Level level, Entity entity, int slot, boolean selected, CallbackInfo ci) {
         if (level.isClientSide() || !EquipmentBenediction.QUALITY_DATA.isValid(stack)) return;
@@ -37,4 +40,29 @@ public class ItemMixin {
             filteredGlossary.forEach(glossaryData -> glossaryCap.addGlossaryID(glossaryData.getId()));
         });
     }
+
+    @Inject(method = "inventoryTick", at = @At(value = "HEAD"))
+    public void addCurios(ItemStack stack, Level level, Entity entity, int slot, boolean selected, CallbackInfo ci) {
+        if (!ModList.get().isLoaded("curios")) {
+            return;
+        }
+        ICuriosHelper curiosHelper = CuriosApi.getCuriosHelper();
+        stack.getCapability(CapabilityRegistry.GLOSSARY).ifPresent(glossaryCap -> {
+            List<String> glossaryIDList = glossaryCap.getGlossaryIDList();
+            glossaryIDList.stream()
+                    .map(EquipmentBenediction.GLOSSARY_DATA::get)
+                    .flatMap(glossaryData -> glossaryData.getAttribute().stream())
+                    .forEach(data -> {
+
+                        Set<String> curioTags = curiosHelper.getCurioTags(stack.getItem());
+                        data.getSlots().stream()
+                            .filter(curioTags::contains)
+                            .forEach(slotType -> data.getAttributeMap().forEach((attribute, attributeModifier) -> {
+                                curiosHelper.addModifier(stack, attribute, attributeModifier.getName(), attributeModifier.getId(), attributeModifier.getAmount(), attributeModifier.getOperation(), slotType);
+                            }));
+                    });
+        });
+    }
+
+
 }
